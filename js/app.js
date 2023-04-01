e0dc3777035d451ac98b989452d1cc1830b37c48
sergiocouter <sergiocouter@gmail.com>
2023-04-01 14:55:11 -0300

    Criada a media query

--- js/app.js
+++ js/app.js
@@ -1,2 +1,5 @@
-let pagamento = document.querySelector(".container__calculo__pagamento__input");
 let comissao = document.querySelectorAll(".container__calculo__comissao__botao");
+
+for (let index = 0; index < comissao.length; index++)  {
+    console.log(index)
+}
