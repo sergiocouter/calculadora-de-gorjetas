af51347ced1d0c236cb97f52b36d8f5e300f6cf3
sergiocouter <sergiocouter@gmail.com>
2023-04-02 11:00:32 -0300

    Desenvolvendo a lógica

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
-let comissao = document.querySelectorAll(".container__calculo__comissao__botao");
+let comissaoTotal = document.querySelector("#preco-total");
+let comissaoPessoa = document.querySelector("#preco-pessoa");
 
-for (let index = 0; index < comissao.length; index++)  {
-    console.log(index)
+let botao5 = document.querySelector("#botao5");
+let botao10 = document.querySelector("#botao10");
+let botao15 = document.querySelector("#botao15");
+let botao25 = document.querySelector("#botao25");
+let botao50 = document.querySelector("#botao50");
+let botao75 = document.querySelector("#botao75");
+
+function calculaGorjeta5() {
+    let valor = document.querySelector("#valor-pagamento");
+    let pessoas = document.querySelector("#numero-pessoas");
+
+    if (valor.value !=="" && pessoas.value !==""){
+
+        let valorGorjetaPessoa =((valor.value * 5) /100)/pessoas.value;
+        let valorGorjetaTotal = (valor.value/pessoas.value) + valorGorjetaPessoa;
+
+        comissaoPessoa.innerHTML=`\$ ${valorGorjetaPessoa.toFixed(2)}`;
+        comissaoTotal.innerHTML= `\$ ${valorGorjetaTotal.toFixed(2)}`;
+        console.log(valor.value)
+    }else{
+        alert('Preencha os campos "Pagamento" e "Número de pessoas" primeiro.')
+    }
 }
+
+botao5.addEventListener('click', () =>{
+    calculaGorjeta5();
+})
